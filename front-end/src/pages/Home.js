5519ab80a87cfed484f5fb44f4ec7070980526af
Andrew Lai <the.andrew.lai@gmail.com>
2023-01-21 14:13:27 -0800

    Added styling to home

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,8 +1,18 @@
+import "../styles/Home.css"
+
 export default function Home() {
 
     return(
         <>
-
+        <div class="div-intro">
+            <section>
+                <h1>Hello.</h1>
+                <h2>Hi All, 1) I have posted solutions to today's quiz, and solutions to all of the questions in quiz assignment 2. Please review the solutions to assignment 2.
+                </h2>
+            </section>
+            
+        </div>
+           
         </>
     )
 }   
--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -3,7 +3,7 @@ export default function Login () {
     return (
         <>
             <div>
-                hello
+                
             </div>
         </>
     )
