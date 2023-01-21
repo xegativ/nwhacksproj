d592e21472a9bcfe594c29ae7c1818662991b9ec
Andrew Lai <the.andrew.lai@gmail.com>
2023-01-21 13:15:35 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+#from django.db import models
+"""
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+"""
+
+class User():
+    def __init__(self, firstName, lastName, userName):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.userName = userName 
+
+
+class Closet():
+    # Closet per user; contains list of clothes
+    def __init__(self, closet_name, desc="", cl_lst=[]):
+        self.cl_lst = cl_lst 
+    
+    def addTop(self, name, desc="", colour="#ffffff", sleeves=True, clean=True):
+        self.cl_lst.append(Top(name, desc, colour, sleeves, clean))
+
+    def addBottom(self, name, desc="", colour="#ffffff", clean=True):
+        self.cl_lst.append(Bottom(name, desc, colour, clean))
+
+    def addShoes(self, name, desc="", colour="#ffffff", clean=True):
+        self.cl_lst.append(Shoes(name, desc, colour, clean))
 
 class Clothing():
     def __init__(self, name, desc="", colour="#ffffff", clean=True):
@@ -5,7 +33,6 @@ class Clothing():
         self.desc = desc
         self.colour = colour
         self.clean = clean
-
 
 class Top(Clothing):
     def __init__(self, name, desc="", colour="#ffffff", sleeves=True, clean=True):
@@ -21,11 +48,8 @@ class Shoes(Clothing):
         super().__init__(name, desc, colour, clean)
         
 
-
-
-
 def main():
-    pass
+    return 0
 
 if __name__ == "__main__":
     main()
