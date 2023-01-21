9e3f32ed7249da47fdae78678f21ae8f10103726
Andrew Lai <the.andrew.lai@gmail.com>
2023-01-21 12:47:26 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 
 class Clothing():
-    def __init__(self, name, clothing_type, desc="", colour="#ffffff", clean=True):
+    def __init__(self, name, desc="", colour="#ffffff", clean=True):
         self.name = name
         self.desc = desc
-        self.clothing_type = clothing_type.upper()
         self.colour = colour
         self.clean = clean
 
 
 class Top(Clothing):
     def __init__(self, name, desc="", colour="#ffffff", sleeves=True, clean=True):
-        super().__init__(name, "TOP", desc, colour, clean)
+        super().__init__(name, desc, colour, clean)
         self.sleeves = sleeves
 
 class Bottom(Clothing):
     def __init__(self, name, desc="", colour="#ffffff", clean=True):
-        super().__init__(name, "BOTTOM", desc, colour, clean)
+        super().__init__(name, desc, colour, clean)
 
 class Shoes(Clothing):
     def __init__(self, name, desc="", colour="#ffffff", clean=True):
-        super().__init__(name, "SHOES", desc, colour, clean)
+        super().__init__(name, desc, colour, clean)
         
 
 
